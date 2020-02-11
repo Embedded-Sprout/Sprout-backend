28581fdefdeff6bb5ffb979064ea4e985dcaf27a
olly-larkin <ollylemonlarkin@gmail.com>
2020-02-11 16:35:23 +0000

    updated to localhost

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 })
 
-var server = app.listen(process.env.PORT, function () {
+// var server = app.listen(process.env.PORT, function () {
+var server = app.listen(5000, function() {
     var host = server.address().address
     var port = server.address().port
    
